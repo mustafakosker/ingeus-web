a3b453c8d0c92bfb285e1882bd376791d1f55a57
Mustafa <kosker@nextbytes.co.uk>
2018-05-02 02:22:01 +0100

    UserForm page

---
+++ src/components/user/UserForm.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+import {
+  DatePicker,
+  TextField
+} from 'redux-form-material-ui';
+
+const required = value => (value == null ? 'Required' : undefined);
+
+class UserForm extends Component {
+  render() {
+    const { handleSubmit, pristine, reset, submitting } = this.props;
+
+    return (
+      <form onSubmit={handleSubmit}>
+        <div>
+          <Field
+            name="firstName"
+            component={TextField}
+            hintText="Last Name"
+            floatingLabelText="Last Name"
+            validate={required}
+            ref="firstName"
+            withRef
+          />
+        </div>
+        <div>
+          <Field
+            name="lastName"
+            component={TextField}
+            hintText="Name"
+            floatingLabelText="Name"
+            validate={required}
+          />
+        </div>
+        <div>
+          <Field
+            name="password"
+            component={TextField}
+            hintText="Name"
+            floatingLabelText="Name"
+            validate={required}
+          />
+        </div>
+        <div>
+          <Field
+            name="dateOfBirth"
+            component={DatePicker}
+            format={null}
+            hintText="Date of Birth"
+            validate={required}
+          />
+        </div>
+        <div>
+          <button type="submit" disabled={submitting}>Submit</button>
+          <button
+            type="button"
+            disabled={pristine || submitting}
+            onClick={reset}
+          >
+            Clear
+          </button>
+        </div>
+      </form>
+    );
+  }
+}
+
+UserForm = reduxForm({
+  form: 'userForm'
+})(UserForm);
+
+export default UserForm;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import App from './App';
+import rootReducer from './reducer/rootReducer';
+import './index.css';
+
+const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
+
+const store = createStoreWithMiddleware(rootReducer);
+
+ReactDOM.render(
+  <Provider
+    store={store}
+  >
+    <App/>
+  </Provider>, document.getElementById('root')
+);
